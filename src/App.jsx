57ca63de6b25668d38d6875e7831f4ff789cf8b8
msibitebogo55-bit <msibitebogo55@gmail.com>
2025-09-08 04:13:42 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Live from "./pages/Live";
 import Upload from "./pages/Upload";
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/live" element={<Live />} />
         <Route path="/upload" element={<Upload />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
