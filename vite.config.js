8eda74efb332d80f958b3c3657e2530dac6bc864
msibitebogo55-bit <msibitebogo55@gmail.com>
2025-09-08 04:14:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./" ,
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
+  base: "/", // Ensure SPA routing works
+});
