6f282668ba8de7822a45830d1497594248783cc6
msibitebogo55-bit <msibitebogo55@gmail.com>
2026-02-02 13:33:13 +0200

    Delete backend directory

--- backend/services/buildFiveCandleSet.js
+++
@@ -1,8 +0,0 @@
-import { analyzeCandle } from "./candleFeatures.js";
-
-export function buildFiveCandleSet(candles) {
-  return candles.map((c, i) => ({
-    candle: i + 1,
-    ...analyzeCandle(c, candles[i - 1])
-  }));
-}
